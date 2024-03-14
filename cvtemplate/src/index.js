8d98a7637d1a29500efeeffa43d4227f4377f937
Your Name <you@example.com>
2024-03-14 13:23:35 +0200

    curs1

---
+++ cvtemplate/src/index.js
@@ -0,0 +1,6 @@
+import './scss/styles.scss'
+import * as bootstrap from 'bootstrap'
+import particles from 'particles.js'
+import './js/particles.js'
+import './js/main.js'
+import '../node_modules/typed.js/dist/typed.umd.js'
